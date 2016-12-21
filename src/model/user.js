8252537be2b7905aaac49c8dad3727ecb71f8a69
Liam Crewe <liam.j.crewe@gmail.com>
2016-12-21 19:11:30 +0000

    Add user to archive table before deleting

--- src/model/user.js
+++ src/model/user.js
@@ -117,19 +117,37 @@ export const remove = (id, callback) => {
       return handleDBErr(err, connection, callback)
     }
 
-    connection.query(
-      'DELETE FROM user WHERE id = ?',
-      [id],
-      (err, result) => {
-        /* istanbul ignore if */
-        if (err) {
-          return handleDBErr(err, connection, callback)
-        }
-
-        connection.release()
-
-        callback(false)
+    getById(id, (err, user) => {
+      /* istanbul ignore if */
+      if (err) {
+        return handleDBErr(err, connection, callback)
       }
-    )
+
+      connection.query(
+        'INSERT INTO user_archive (id, email, password) VALUES (?, ?, ?)',
+        [id, user.email, user.password],
+        (err, result) => {
+          /* istanbul ignore if */
+          if (err) {
+            return handleDBErr(err, connection, callback)
+          }
+
+          connection.query(
+            'DELETE FROM user WHERE id = ?',
+            [id],
+            (err, result) => {
+              /* istanbul ignore if */
+              if (err) {
+                return handleDBErr(err, connection, callback)
+              }
+
+              connection.release()
+
+              callback(false)
+            }
+          )
+        }
+      )
+    })
   })
 }
