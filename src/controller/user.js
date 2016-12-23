574dd580de3494603d3fbf285726578425d50467
Liam Crewe <liam.j.crewe@gmail.com>
2016-12-23 12:45:16 +0000

    Simplified err handling. No need to he 3 lines

--- src/controller/user.js
+++ src/controller/user.js
@@ -13,32 +13,24 @@ export const create = (body, res) => {
   const password = body.password // to be hashed
 
   if (!email || !password) {
-    res.status(400).json({ error: 'Missing email or password field(s)' })
-
-    return
+    return res.status(400).json({ error: 'Missing email or password field(s)' })
   }
 
   getUserByEmail(email, (err, user) => {
     /* istanbul ignore if */
     if (err) {
-      res.status(500).json({ error: 'DB error' })
-
-      return
+      return res.status(500).json({ error: 'DB error' })
     }
 
     // User already exists
     if (user) {
-      res.status(409).json({ error: 'Email already in use' })
-
-      return
+      return res.status(409).json({ error: 'Email already in use' })
     }
 
     createUser(email, password, (err, user) => {
       /* istanbul ignore if */
       if (err) {
-        res.status(500).json({ error: 'DB error' })
-
-        return
+        return res.status(500).json({ error: 'DB error' })
       }
 
       // Don't want to send password back
@@ -55,15 +47,11 @@ export const get = (id, res) => {
   getUserById(id, (err, user) => {
     /* istanbul ignore if */
     if (err) {
-      res.status(500).json({ error: 'DB error' })
-
-      return
+      return res.status(500).json({ error: 'DB error' })
     }
 
     if (!user) {
-      res.status(404).json({ error: 'No user found with this id' })
-
-      return
+      return res.status(404).json({ error: 'No user found with this id' })
     }
 
     // Don't want to send password back
@@ -79,39 +67,29 @@ export const put = (id, body, res) => {
   const newPassword = body.newPassword // to be hashed
 
   if (!email || !oldPassword || !newPassword) {
-    res.status(400).json({ error: 'Missing email or password field(s)' })
-
-    return
+    return res.status(400).json({ error: 'Missing email or password field(s)' })
   }
 
   getUserById(id, (err, user) => {
     /* istanbul ignore if */
     if (err) {
-      res.status(500).json({ error: 'DB error' })
-
-      return
+      return res.status(500).json({ error: 'DB error' })
     }
 
     bcrypt.compare(oldPassword, user.password, (err, isMatch) => {
       /* istanbul ignore if */
       if (err) {
-        res.status(500).json({ error: 'An error occurred' })
-
-        return
+        return res.status(500).json({ error: 'An error occurred' })
       }
 
       if (!isMatch) {
-        res.status(401).json({ message: 'Invalid password' })
-
-        return
+        return res.status(401).json({ message: 'Invalid password' })
       }
 
       putUser(id, email, newPassword, (err, user) => {
         /* istanbul ignore if */
         if (err) {
-          res.status(500).json({ error: 'DB error' })
-
-          return
+          return res.status(500).json({ error: 'DB error' })
         }
 
         // Don't want to send password back
@@ -127,23 +105,17 @@ export const remove = (id, res) => {
   getUserById(id, (err, user) => {
     /* istanbul ignore if */
     if (err) {
-      res.status(500).json({ error: 'DB error' })
-
-      return
+      return res.status(500).json({ error: 'DB error' })
     }
 
     if (!user) {
-      res.status(404).json({ error: 'No user found with this id' })
-
-      return
+      return res.status(404).json({ error: 'No user found with this id' })
     }
 
     removeUser(user, err => {
       /* istanbul ignore if */
       if (err) {
-        res.status(500).json({ error: 'DB error' })
-
-        return
+        return res.status(500).json({ error: 'DB error' })
       }
 
       res.status(200).json({ message: 'User deleted' })
